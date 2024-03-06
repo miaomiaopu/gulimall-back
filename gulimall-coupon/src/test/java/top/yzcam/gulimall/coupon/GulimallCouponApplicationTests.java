5ba7b87e565672f95c90e333f8af2e0505521b85
vcat <1991416497@qq.com>
2024-03-06 13:59:24 +0800

    创建初始项目结构

---
+++ gulimall-coupon/src/test/java/top/yzcam/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yzcam.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
